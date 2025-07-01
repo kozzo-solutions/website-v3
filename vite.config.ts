a69798e211714e6d59877b1f0ce114e5cba7201b
Etiarse <et.arsenault2000@gmail.com>
2025-07-01 18:17:05 -0400

    Fix json

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: '/kozzo-website-v3/',
+  base: '/website-v3/',
   plugins: [
     react(),
   ],
